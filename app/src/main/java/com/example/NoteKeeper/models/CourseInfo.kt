ca22bd5e3259213cdfd221c71e20a842b9ef6c9f
sat939 <vsponakala@gmail.com>
2021-10-05 22:37:58 -0400

    Initial commit -sp

---
+++ app/src/main/java/com/example/NoteKeeper/models/CourseInfo.kt
@@ -0,0 +1,7 @@
+package com.example.NoteKeeper.models
+
+data class CourseInfo(
+    val courseId: Int,
+    val name: String,
+    val departmentName: String
+)
---
+++ app/src/main/java/com/example/NoteKeeper/models/Notes.kt
@@ -0,0 +1,7 @@
+package com.example.NoteKeeper.models
+
+data class Notes(
+    val courseInfo: CourseInfo ,
+    val title: String,
+    val text: String
+)
